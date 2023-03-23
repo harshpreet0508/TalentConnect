5fe19c985866a5969f2a644fa8a2945a1505a611
Krishna, Madgula S <madgula_krishna@optum.com>
2023-03-23 15:35:43 +0530

    fix instead of formcontrol

--- Talent-Connect/src/app/hiring-manager/edit-record/edit-record.component.ts
+++ Talent-Connect/src/app/hiring-manager/edit-record/edit-record.component.ts
@@ -13,48 +13,19 @@ import { JobRequest } from 'src/app/models/JobRequest.model';
 })
 export class EditRecordComponent {
   constructor(private hm: HiringManagerService, private http: HttpClient, private activatedRoute: ActivatedRoute) { }
-  editForm =  new FormGroup({
-    reqNumber: new FormControl(null),
-    grade: new FormControl('')
-  });
   jobRequest:JobRequest;
   userid!: number;
   ngOnInit(): void {
-    // const id = this.router.snapshot.paramMap.get('id');
-    // // console.log(id);
-    // this.hm.getJobRequestDetailsById(id)
-    //   .subscribe((result:JobRequest) => {
-    //     console.log("result:",result);
-    //     this.jobRequest=result[0];
-    //     const id = this.jobRequest.reqNumber;
-    //     console.log(id);
-    //     // console.log(this.reqNumber);
-    //     this.editForm = new FormGroup({
-    //       reqNumber: new FormControl(this.reqNumber),
-    //       grade: new FormControl("")
-    //     });
-    //   });
-    // this.editForm=this.fb.group({})
     this.activatedRoute.params.subscribe(val => {
       this.userid = val['id'];
-      this.hm.getJobRequestDetailsById(this.userid).subscribe(res=>{
+      this.hm.getJobRequestDetailsById(this.userid).subscribe((res:JobRequest)=>{
         console.log(res.reqNumber);
-        // this.editForm.setValue({reqNumber:res.reqNumber,
-        //   grade:res.grade,}) ;
+        this.jobRequest =res;
+        console.log("jobRequest===",this.jobRequest)
       })
     })
   }
 
   updateData() {
-    // console.log(this.editForm.value);
-    // this.http.put('http://localhost:8080/talentConnect/api/talentAcquisition/saveJobRequestDetails', this.registerForm.value)
-    //   .subscribe(status => console.log(JSON.stringify(status)));
-    // alert("form submitted");
-  }
-  get reqNumber(): FormControl {
-    return this.editForm.get("reqNumber") as FormControl;
-  }
-  get Grade(): FormControl {
-    return this.editForm.get("grade") as FormControl;
   }
 }
